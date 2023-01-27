1205d4eb7a29d1efeafcef0daec3cd50fa763544
VijjapuSandeep <104676554+VijjapuSandeep@users.noreply.github.com>
2023-01-27 10:45:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,57 +1,72 @@
 
-import React, { useState } from 'react'
-import DATA from './API.json'
-import "./App.css";
+// import React, { useState } from 'react'
+// import DATA from './API.json'
+// import "./App.css";
 
-const Search = () => {
-  const [filter, setFilter] = useState('');
-  const SearchText = (event) => {
-    setFilter(event.target.value);
-  }
-  let dataSearch = DATA.filter(item => {
-    return Object.keys(item).some(key =>
-      item[key].toString().includes(filter.toString()))
-  });
-  const onClickHandler=()=>{
-    window.alert('Print Successful');
-  }
-  return (
-    <section className='py-4 container'>
-      <h1 className="heading">React Sports Search</h1>
-      <div className="searchitems">
-        <input type="text" className='app__input' placeholder='Sports Name' value={filter} onChange={SearchText.bind(this)} />
-        <input className="app__submit" type="submit" value="Search" />
-      </div>
-      <div className="spanElements">
-        <div><span> Search results for - { filter }</span></div>
-        <div><span>Count - { dataSearch.length}</span></div>
-      </div>
+// const Search = () => {
+//   const [filter, setFilter] = useState('');
+//   const SearchText = (event) => {
+//     setFilter(event.target.value);
+//   }
+//   let dataSearch = DATA.filter(item => {
+//     return Object.keys(item).some(key =>
+//       item[key].toString().includes(filter.toString()))
+//   });
+//   const onClickHandler=()=>{
+//     window.alert('Print Successful');
+//   }
+//   return (
+//     <section className='py-4 container'>
+//       <h1 className="heading">React Sports Search</h1>
+//       <div className="searchitems">
+//         <input type="text" className='app__input' placeholder='Sports Name' value={filter} onChange={SearchText.bind(this)} />
+//         <input className="app__submit" type="submit" value="Search" />
+//       </div>
+//       <div className="spanElements">
+//         <div><span> Search results for - { filter }</span></div>
+//         <div><span>Count - { dataSearch.length}</span></div>
+//       </div>
       
-      <div className="col-md-14">
-        <div className="row">
-          {dataSearch.map((item, index) => {
-            return (
-              <>
-                <div className="col-12" key={item.id}>
-                  <div className="card">
-                    <img src={item.image} className="card-img-top" alt="..." />
-                    <div className="card-body">
-                      <div className="print-container">
-                        <h4 className="card-title">{item.label}</h4>
-                        <button type="submit" onClick={window.print}>Print</button>
-                      </div>
+//       <div className="col-md-14">
+//         <div className="row">
+//           {dataSearch.map((item, index) => {
+//             return (
+//               <>
+//                 <div className="col-12" key={item.id}>
+//                   <div className="card">
+//                     <img src={item.image} className="card-img-top" alt="..." />
+//                     <div className="card-body">
+//                       <div className="print-container">
+//                         <h4 className="card-title">{item.label}</h4>
+//                         <button type="submit" onClick={window.print}>Print</button>
+//                       </div>
                       
-                      <p className="card-para">{item.description}</p>
-                    </div>
-                  </div>
-                </div>
-              </>
-            )
-          })}
-        </div>
-      </div>
-    </section>
-  )
+//                       <p className="card-para">{item.description}</p>
+//                     </div>
+//                   </div>
+//                 </div>
+//               </>
+//             )
+//           })}
+//         </div>
+//       </div>
+//     </section>
+//   )
+// }
+
+// export default Search;
+
+import './App.css';
+import React from 'react';
+import Search from './components/Search';
+
+function App() {
+  
+  return (
+    <div className="App">
+      <Search></Search>
+    </div>
+  );
 }
 
-export default Search;
+export default App;
